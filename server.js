dd3749f777033a5ce0feccaa484e128c972af3bd
Tristan Galloway <tristan.a.galloway@gmail.com>
2025-01-29 12:43:17 -0700

    Finished: MVC Getting Started

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * View Engine and Templates
@@ -25,9 +26,7 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
